a735d75638924eed287bf81ced610cdf67ab2343
Arukenofu <balkenov711@gmail.com>
2025-05-16 16:25:13 +0500

    added logs

--- src/commands/patch-image.ts
+++ src/commands/patch-image.ts
@@ -9,20 +9,25 @@ const patchImage = () => async (
   if (!('entities' in context.message!) || !('payload' in context)) {
     return;
   }
+  console.log('validation passed');
 
   const entities = context.message!.entities!;
   const stickersLink = entities
     .filter(entry => entry.type === 'custom_emoji')
     .map(entry => entry.custom_emoji_id);
   const stickers = await getStickersLink(context.telegram, stickersLink);
+  console.log('stickers passed');
 
   const formattedHtml = formatPatchNotes(String(context.payload), {
     lineBreak: false,
     wrapParagraph: true
   });
+  console.log('html formatting passed');
   const html = replaceEmojisWithStickers(formattedHtml, stickers);
+  console.log('stickers replace passed');
 
   const screenshot = await htmlToImage(html);
+  console.log('screenshot passed');
 
   await context.sendDocument({source: Buffer.from(new Uint8Array(screenshot.buffer)), filename: 'photo.png'})
 }
