003419b9998710eb99fad30148fd17e32fd6b347
Arukenofu <balkenov711@gmail.com>
2025-05-16 16:43:59 +0500

    added more logs

--- src/actions/html-to-image/index.ts
+++ src/actions/html-to-image/index.ts
@@ -4,7 +4,9 @@ import * as path from 'node:path';
 
 async function htmlToImage(htmlContent: string) {
   const browser = await puppeteer.launch();
+  console.log('browser started');
   const page = await browser.newPage();
+  console.log('page created');
 
   const cssPath = path.resolve(__dirname, 'styles.css');
 
@@ -23,11 +25,15 @@ async function htmlToImage(htmlContent: string) {
     </html>`
 
   await page.setContent(finalHtml, { waitUntil: 'networkidle0' });
+  console.log('html content was set');
   await page.setViewport({ width: 768, height: 1024 });
+  console.log('viewport was set');
 
   const screenshot = await page.screenshot({fullPage: true});
+  console.log('screenshot created');
 
   await browser.close();
+  console.log('browser closed');
 
   return screenshot;
 }
