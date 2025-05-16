caa16f24ee202a6fa064d129cd514982c03065b1
Arukenofu <balkenov711@gmail.com>
2025-05-17 02:36:02 +0500

    some optimizations

--- src/shared/helpers/create-browser.ts
+++ src/shared/helpers/create-browser.ts
@@ -1,20 +1,20 @@
-import * as puppeteer from 'puppeteer';
-
-// @ts-ignore
-import chromium from '@sparticuz/chromium';
-import puppeteerCore from 'puppeteer-core';
-
 async function createBrowser() {
   const ENVIRONMENT = process.env.NODE_ENV || '';
 
   if (ENVIRONMENT === 'production') {
-    return puppeteerCore.launch({
+    const puppeteer = await import('puppeteer-core')
+    // @ts-ignore
+    const chromium = (await import('@sparticuz/chromium')).default;
+
+    return puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
   } else {
+    const puppeteer = await import('puppeteer')
+
     return puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       headless: true,
