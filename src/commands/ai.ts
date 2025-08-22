7046f80b1e1b7b357b26db140c67998eb75f5c6f
Arukenofu <balkenov711@gmail.com>
2025-08-22 23:23:47 +0500

    update: ai

--- src/commands/ai.ts
+++ src/commands/ai.ts
@@ -1,20 +1,29 @@
 import { Context } from 'telegraf';
 import { AIService } from '../services/AI';
 import { splitMessage } from '../shared/helpers/split-message';
+import { getPhotolink } from '../shared/helpers/get-photolink';
+import { fetchImageAsBase64 } from '../shared/helpers/base64';
 
 const aiCommand = () => async (
   context: Context,
 ) => {
   const ai = new AIService(process.env.AI_SERVICE_KEY, {
-    scenario: 'Отвечай без никакого форматирования'
+    scenario: 'Отвечай без никакого форматирования',
   });
 
-  const data = await ai.sendText(context.text || 'Привет!');
-  const response = data.candidates[0].content.parts[0].text;
+  let responseText: string;
 
-  const messages = splitMessage(response);
+  if (context.message && 'photo' in context.message) {
+    const link = await getPhotolink(context.message.photo, context.telegram);
+    const base64 = await fetchImageAsBase64(link, 'image/jpeg');
+    const data = await ai.sendImage(base64, context.text || '');
+    responseText = data.candidates[0].content.parts[0].text;
+  } else {
+    const data = await ai.sendText(context.text || 'Привет!');
+    responseText = data.candidates[0].content.parts[0].text;
+  }
 
-  for (const msg of messages) {
+  for (const msg of splitMessage(responseText)) {
     await context.reply(msg);
   }
 }
